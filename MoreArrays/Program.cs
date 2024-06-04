6fc31a0e43e371377e8c1ed5ffc4551e5a0d9d77
Tayrific <103253195+Tayrific@users.noreply.github.com>
2024-06-04 15:21:58 +0100

    More arrays exercise 2 complete (big O notation)

--- MoreArrays/Program.cs
+++ MoreArrays/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MoreArrays
+﻿using System.ComponentModel.Design;
+
+namespace MoreArrays
 {
     internal class Program
     {
@@ -11,10 +13,33 @@
             }
             return total;
         }
+
+        public static int SequentialSearch(int[] arr, int size, int value)
+        {
+            int numFound;
+            for (int i = 0; i < size; i++)
+            {
+                if (arr[i] == value)
+                {
+                    numFound = arr[i];
+                    Console.WriteLine("value was found!");
+                    return numFound;
+                }
+            }
+            Console.WriteLine("Value was not found!");
+            return -1;
+        }
+
         public static void Main(string[] args)
         {
             int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             Console.WriteLine("Sum of values in array: {0}", SumArray(arr));
+            Console.WriteLine("--------------------------------");
+
+            Console.Write("Enter a number to search for within array: ");
+            int searchNum = Convert.ToInt32(Console.ReadLine());
+            SequentialSearch(arr, arr.Length, searchNum);
+
             Console.ReadLine();
         }
 
