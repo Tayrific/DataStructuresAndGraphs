35b2f94c25021d0d188956933074207a8358227a
Tayrific <103253195+Tayrific@users.noreply.github.com>
2024-06-04 11:16:55 +0100

    Expression Tree

---
+++ ExpressionTreeProject/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionTreeProject
+{
+    class Node
+    {
+        public Node lchild;
+        public char info;
+        public Node rchild;
+
+        public Node(char c)
+        {
+            info = c;
+            lchild = null;
+            rchild = null;
+        }
+    }
+}
+
---
+++ ShellSort/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ShellSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--SHELL SORT--");
+            int[] array = new int[] { 9, 1, 8, 2, 7, 3, 6, 4, 5 };
+
+            Console.Write("unsorted List: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            SelectionSort(array);
+
+            Console.Write("\nsorted List: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
