7c0808141367b455816b73b07658d7d8c7973f9f
Tayrific <103253195+Tayrific@users.noreply.github.com>
2024-06-04 11:35:22 +0100

    Shell sort

--- ShellSort/Program.cs
+++ ShellSort/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ShellSort
+﻿using System.Buffers;
+
+namespace ShellSort
 {
     internal class Program
     {
@@ -6,20 +8,60 @@
         {
             Console.WriteLine("--SHELL SORT--");
             int[] array = new int[] { 9, 1, 8, 2, 7, 3, 6, 4, 5 };
+            Console.Write("Unsorted List:");
+            Arr_element(array);
+            int size = array.Length;
 
-            Console.Write("unsorted List: ");
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.Write(array[i] + " ");
-            }
+            ShellSort(array, size);
 
-            Console.Write("\nsorted List: ");
-            for (int i = 0; i < array.Length; i++)
+            Console.Write("\nSorted List: ");
+            Arr_element(array);
+
+            Console.ReadLine();
+        }
+
+        private static void Arr_element(int[] array)
+        {
+            foreach (var elem in array)
             {
-                Console.Write(array[i] + " ");
+                Console.Write(elem + " ");
             }
             Console.WriteLine();
-            Console.ReadLine();
+
+        }
+
+        private static void ShellSort(int[] array, int size)
+        {
+            int i, j, inc, temp;
+            inc = 3;
+
+            while(inc>0)
+            {
+                for(i = 0; i < size; i++)
+                {
+                    j = i;
+                    temp = array[i];
+                    while((j>=inc) && (array[j-inc]> temp)) // checking the values
+                    {
+                        array[j] = array[j-inc];
+                        j = j - inc;
+
+                    }
+                    array[j] = temp; //save to array
+                }
+                if (inc/2 != 0)
+                {
+                    inc = inc / 2;
+                } 
+                else if(inc == 1)
+                {
+                    inc = 0;
+                } 
+                else
+                {
+                    inc = 1;
+                }
+            }
         }
     }
 }
