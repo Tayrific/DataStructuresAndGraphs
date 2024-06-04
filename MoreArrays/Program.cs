2e8b9b29d3890cb125c50938af32814da5f995be
Tayrific <103253195+Tayrific@users.noreply.github.com>
2024-06-04 15:11:57 +0100

    More arrays exercise 1 (within big O notation)

---
+++ MoreArrays/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace MoreArrays
+{
+    internal class Program
+    {
+        public static int SumArray(int[] arr)
+        {
+            int total = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                total += arr[i];
+            }
+            return total;
+        }
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.WriteLine("Sum of values in array: {0}", SumArray(arr));
+            Console.ReadLine();
+        }
+
+
+    }
+}
+
