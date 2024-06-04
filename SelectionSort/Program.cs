d0756b9446e6b2a1590d9f1e6f695f787d2f28cd
Tayrific <103253195+Tayrific@users.noreply.github.com>
2024-06-04 11:17:13 +0100

    Selection sort complete

--- SelectionSort/Program.cs
+++ SelectionSort/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("--SLECTION SORT--");
+            Console.WriteLine("--SELECTION SORT--");
             int[] array = new int[] { 9, 1, 8, 2, 7, 3, 6, 4, 5 };
 
             Console.Write("unsorted List: ");
